361be5062c97591166ebbc5c00dddc446da5849c
Chris Tolan <tolancode@gmail.com>
2024-01-15 14:17:44 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import HomePage from './HomePage'
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewPage from './NewPage'
 
 function App() {
   return (
-  
+  <Router>
     <Routes>
       <Route path="/" exact component={HomePage} />
       <Route path="/new-page" component= {NewPage}/>
     </Routes>
+  </Router>
     
   )
 }
