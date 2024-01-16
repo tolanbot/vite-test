b9fef11848f6dab5609b4801ba3ed1587ee66590
Chris Tolan <tolancode@gmail.com>
2024-01-16 12:18:56 -0800

    working add items form

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,24 +1,27 @@
 import LikeButton from "./like-button";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ToDoList() {
-  const [isDone1, setIsDone1] = useState(false);
-  const [isDone2, setIsDone2] = useState(false);
-  const [isDone3, setIsDone3] = useState(false);
+  const [items, setItems] = useState([]);
 
-  const handleItemClick = () => {
-    setIsDone1(!isDone1);
+  const handleItemClick = (index) => {
+    const newItems = [...items];
+    newItems[index].isDone = !newItems[index].isDone;
+    setItems(newItems);
   };
 
-  const handleItemClick2 = () => {
-    setIsDone2(!isDone2);
+  const addItem = (itemName) => {
+    setItems([...items, { name: itemName, isDone: false }]);
   };
 
-  const handleItemClick3 = () => {
-    setIsDone3(!isDone3);
-  };
+  useEffect(() => {
+    if (items.length > 0 && items.every((item) => item.isDone)) {
+      console.log("All Items Completed");
+    }
+  }, [items]);
+
   return (
     <>
       <h1>Hedy Lamarr&apos;s Todos</h1>
@@ -27,29 +30,16 @@ export default function ToDoList() {
         alt="Hedy Lamarr"
         className="photo"
       />
+      <AddItemForm onAddItem={addItem} />
       <ul>
-        <Item
-          className="item1"
-          name="Invent New Traffic Lights"
-          isDone={isDone1}
-          clickFunc={handleItemClick}
-        />
-
-        <Item
-          className="item2"
-          name="Rehearse a movie scene"
-          isDone={isDone2}
-          clickFunc={handleItemClick2}
-        />
-
-        <Item
-          className="item3"
-          name="Improve the spectrum technology"
-          isDone={isDone3}
-          clickFunc={handleItemClick3}
-        />
-
-        <Item2 name="Test The List" />
+        {items.map((item, index) => (
+          <Item
+            key={index}
+            name={item.name}
+            isDone={item.isDone}
+            clickFunc={() => handleItemClick(index)}
+          />
+        ))}
       </ul>
       <br></br>
       <LikeButton />
@@ -84,36 +74,65 @@ Item.propTypes = {
   clickFunc: PropTypes.func.isRequired,
 };
 
-function Item2({ name }) {
-  const [isItDone, setDone] = useState(false);
-
-  const handleClick = () => {
-    setDone(!isItDone);
+function AddItemForm({ onAddItem }) {
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      onAddItem(inputValue);
+      setInputValue("");
+    }
   };
-
   return (
-    <a className="test" onClick={handleClick}>
-      <li
-        style={{
-          listStyleType: "disc",
-          textAlign: "center",
-          position: "relative",
-        }}
-      >
-        <span style={{ display: "inline-block" }}>{name}</span>
-        {isItDone && (
-          <span style={{ position: "absolute", right: "10%" }}>✅</span>
-        )}
-      </li>
-    </a>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add New Item"
+      ></input>
+      <button type="submit">Add</button>
+    </form>
   );
 }
-
-Item2.propTypes = {
-  //   isDone: PropTypes.bool.isRequired,
-  name: PropTypes.string.isRequired,
-  //   clickFunc: PropTypes.func.isRequired,
+AddItemForm.propTypes = {
+  onAddItem: PropTypes.func.isRequired,
 };
+
+// function Item2({ name }) {
+//   const [isItDone, setDone] = useState(false);
+
+//   const handleClick = () => {
+//     setDone(!isItDone);
+//   };
+
+//   return (
+//     <a className="test" onClick={handleClick}>
+//       <li
+//         style={{
+//           listStyleType: "disc",
+//           textAlign: "center",
+//           position: "relative",
+//         }}
+//       >
+//         <span style={{ display: "inline-block" }}>{name}</span>
+//         {isItDone && (
+//           <span style={{ position: "absolute", right: "10%" }}>✅</span>
+//         )}
+//       </li>
+//     </a>
+//   );
+// }
+
+// Item2.propTypes = {
+//   name: PropTypes.string.isRequired,
+// };
+
+// function addItemForm() {
+//   let itemNum = 1;
+//   const taskArray = [];
+//   return <input type="text"></input>;
+// }
 
 // function Item({ name, isDone }) {
 //   return isDone ? (
@@ -124,3 +143,26 @@ Item2.propTypes = {
 //     <li>{name}</li>
 //   );
 // }
+
+{
+  /* <Item
+className="item1"
+name="Invent New Traffic Lights"
+isDone={isDone1}
+clickFunc={handleItemClick}
+/>
+
+<Item
+className="item2"
+name="Rehearse a movie scene"
+isDone={isDone2}
+clickFunc={handleItemClick2}
+/>
+
+<Item
+className="item3"
+name="Improve the spectrum technology"
+isDone={isDone3}
+clickFunc={handleItemClick3}
+/> */
+}
